3d2d6746a046a91d805dcc8a5d74cd7f93dfb710
andrej <holesa.andrej@gmail.com>
2021-02-11 11:29:28 +0100

    comment downloadJson function; add downloadCSV function

--- admin/src/containers/ExportPage/ExportModel.js
+++ admin/src/containers/ExportPage/ExportModel.js
@@ -1,47 +1,72 @@
-import React, {useState} from 'react';
-import {Button} from "strapi-helper-plugin";
-import {saveAs} from "file-saver";
-import {fetchEntries} from "../../utils/contentApis";
-import {HFlex, ModelItem} from "./ui-components";
+import React, { useState } from "react";
+import { Button } from "strapi-helper-plugin";
+import { saveAs } from "file-saver";
+import { fetchEntries } from "../../utils/contentApis";
+import { HFlex, ModelItem } from "./ui-components";
 import JsonDataDisplay from "../../components/JsonDataDisplay";
 
-const ExportModel = ({model}) => {
+const ExportModel = ({ model }) => {
   const [fetching, setFetching] = useState(false);
   const [content, setContent] = useState(null);
   const fetchModelData = () => {
     setFetching(true);
-    fetchEntries(model.apiID, model.schema.kind).then((data) => {
-      setContent(data);
-    }).finally(() => {
-      setFetching(false);
-    });
+    fetchEntries(model.apiID, model.schema.kind)
+      .then((data) => {
+        setContent(data);
+      })
+      .finally(() => {
+        setFetching(false);
+      });
   };
 
-  const downloadJson = () => {
-    const current = new Date();
-    const file = new File([JSON.stringify(content)],
-      `${model.apiID}-${current.getTime()}.json`,
-      {type: "application/json;charset=utf-8"});
+  // const downloadJson = () => {
+  //   const current = new Date();
+  //   const file = new File(
+  //     [JSON.stringify(content)],
+  //     `${model.apiID}-${current.getTime()}.json`,
+  //     { type: "application/json;charset=utf-8" }
+  //   );
+  //   saveAs(file);
+  // };
+
+  const downloadCsv = () => {
+    const array = [Object.keys(content[0])].concat(content);
+    const convert = array
+      .map((it) => {
+        return Object.values(it).toString();
+      })
+      .join("\n");
+    const file = new File([convert], "file.csv", {
+      type: "text/csv",
+    });
     saveAs(file);
   };
-  return (<ModelItem>
-    <HFlex>
-      <span className='title'>{model.schema.name}</span>
-      <div>
-        <Button disabled={fetching}
-                loader={fetching}
-                onClick={fetchModelData}
-                secondaryHotline>{fetching ? "Fetching" : "Fetch"}</Button>
-        <Button disabled={!content}
-                onClick={downloadJson}
-                kind={content ? 'secondaryHotline' : 'secondary'}
-        >Download</Button>
-      </div>
-    </HFlex>
-    {
-      content && (<JsonDataDisplay data={content}/>)
-    }
-  </ModelItem>)
+
+  return (
+    <ModelItem>
+      <HFlex>
+        <span className="title">{model.schema.name}</span>
+        <div>
+          <Button
+            disabled={fetching}
+            loader={fetching}
+            onClick={fetchModelData}
+            secondaryHotline
+          >
+            {fetching ? "Fetching" : "Fetch"}
+          </Button>
+          <Button
+            disabled={!content}
+            onClick={downloadCsv}
+            kind={content ? "secondaryHotline" : "secondary"}
+          >
+            Download
+          </Button>
+        </div>
+      </HFlex>
+      {content && <JsonDataDisplay data={content} />}
+    </ModelItem>
+  );
 };
 
 export default ExportModel;
